4b96e9309799454c7ae5e87e5ff2f9a18fc8872f
JonathanVandenBoschPXL <jonathan.vandenbosch@student.pxl.be>
2022-10-11 16:31:28 +0200

    Frontend folder with Workflow

---
+++ frontend/src/app/components/confession-item/confession-item.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Confession} from "../../models/confession.model";
+
+@Component({
+  selector: 'app-confession-item',
+  templateUrl: './confession-item.component.html',
+  styleUrls: ['./confession-item.component.css']
+})
+export class ConfessionItemComponent implements OnInit {
+  @Input() confession!: Confession;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
