07e1005d5d941530f787a7077f3a4c0de7a5ada9
JonathanVandenBoschPXL <79832045+JonathanVandenBoschPXL@users.noreply.github.com>
2022-12-02 13:06:44 +0100

    Update tagFixer.sh

--- tagFixer.sh
+++ tagFixer.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -L -X GET 'https://api.github.com/repos/JonathanVandenBoschPXL/Java_HelloWorld/tags' -H 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}'
+#curl -L -X GET 'https://api.github.com/repos/JonathanVandenBoschPXL/Java_HelloWorld/tags' -H 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}'
+curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+  https://api.github.com/repos/JonathanVandenBoschPXL/Java_HelloWorld/tags
